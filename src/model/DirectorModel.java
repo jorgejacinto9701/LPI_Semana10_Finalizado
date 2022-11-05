f059898f139033b6afa3b3a62ca01292b35d40b0
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2022-11-05 09:47:44 -0500

    Ajustes finales

--- src/model/DirectorModel.java
+++ src/model/DirectorModel.java
@@ -2,9 +2,13 @@ package model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import entidad.Director;
+import entidad.Grado;
 import util.MySqlDBConexion;
 
 public class DirectorModel {
@@ -41,4 +45,108 @@ public class DirectorModel {
 		}
 		return salida;
 	}
+	
+	public List<Director> listaTodos(){
+		ArrayList<Director> salida = new ArrayList<Director>();
+		
+		Connection conn = null;
+		PreparedStatement psmt = null;
+		ResultSet rs = null;
+		try {
+			//1 Se crea la conexion
+			conn = MySqlDBConexion.getConexion();
+			
+			//2 Se prepara el SQL
+			String sql = "SELECT c.*, p.nombre FROM Director c inner join Grado p on c.idGrado = p.idGrado";
+			psmt = conn.prepareStatement(sql);
+			
+			log.info(">>> " + psmt);
+			
+			//3 Se ejecuta el SQL en la base de datos
+			rs = psmt.executeQuery();
+			Director objDirector = null;
+			Grado objGrado = null;
+			while(rs.next()) {
+				objDirector = new Director();
+				objDirector.setIdDirector(rs.getInt(1));
+				objDirector.setNombre(rs.getString(2));
+				objDirector.setFechaNacimiento(rs.getDate(3));
+				objDirector.setFechaRegistro(rs.getDate(4));
+				objDirector.setEstado(rs.getInt(5));
+				
+				objGrado = new Grado();
+				objGrado.setIdGrado(rs.getInt(6));
+				objGrado.setNombre(rs.getString(7));
+				objDirector.setGrado(objGrado);
+				salida.add(objDirector);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) rs.close();
+				if (psmt != null) psmt.close();
+				if (conn != null) conn.close();
+			} catch (Exception e2) {}
+		}
+		return salida;
+	}
+	
+	
+	public List<Director> listaPorGrado(int idGrado){
+		ArrayList<Director> salida = new ArrayList<Director>();
+		
+		Connection conn = null;
+		PreparedStatement psmt = null;
+		ResultSet rs = null;
+		try {
+			//1 Se crea la conexion
+			conn = MySqlDBConexion.getConexion();
+			
+			//2 Se prepara el SQL
+			String sql = "SELECT c.*, p.nombre FROM Director c inner join Grado p on c.idGrado = p.idGrado where c.idGrado = ?";
+			psmt = conn.prepareStatement(sql);
+			psmt.setInt(1, idGrado);
+			
+			log.info(">>> " + psmt);
+			
+			//3 Se ejecuta el SQL en la base de datos
+			rs = psmt.executeQuery();
+			Director objDirector = null;
+			Grado objGrado = null;
+			while(rs.next()) {
+				objDirector = new Director();
+				objDirector.setIdDirector(rs.getInt(1));
+				objDirector.setNombre(rs.getString(2));
+				objDirector.setFechaNacimiento(rs.getDate(3));
+				objDirector.setFechaRegistro(rs.getDate(4));
+				objDirector.setEstado(rs.getInt(5));
+				
+				objGrado = new Grado();
+				objGrado.setIdGrado(rs.getInt(6));
+				objGrado.setNombre(rs.getString(7));
+				objDirector.setGrado(objGrado);
+				salida.add(objDirector);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) rs.close();
+				if (psmt != null) psmt.close();
+				if (conn != null) conn.close();
+			} catch (Exception e2) {}
+		}
+		return salida;
+	}
+	
 }
+
+
+
+
+
+
+
